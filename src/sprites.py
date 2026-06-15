467c0bc4059dec5ccf8d9289262f2c73d5e6613c
Bernardo Guedes <bguedessilveira@gmail.com>
2026-06-14 21:24:58 -0300

    feat: implementa movimentação de ambos personagens

--- src/sprites.py
+++ src/sprites.py
@@ -1,21 +1,21 @@
 import pygame
 def pegar_sprite(sheet, x, y, width, height, scale=1):
+    
     """Corta um único elemento de uma spritesheet BMP e remove o fundo."""
     # 1. Carrega o BMP e usa .convert() (sem alpha) para otimizar a velocidade
     # 2. Cria uma superfície padrão para o recorte (não precisa de SRCALPHA aqui)
     image = pygame.Surface((width, height), pygame.SRCALPHA)
     # 3. Copia o pedaço da folha BMP para a nossa nova imagem
     image.blit(sheet, (0, 0), (x, y, width, height))
+
     # 4. CONFIGURAÇÃO DA TRANSPARÊNCIA (O segredo para o BMP)
-    # Pegamos a cor do pixel no canto superior esquerdo (0,0) do recorte, 
-    # assumindo que o fundo do seu sprite começa ali.
-    # Dizemos ao Pygame para ignorar essa cor específica na hora de desenhar
     for px in range(width):
         for py in range(height):
             r, g, b, *_ = image.get_at((px, py))
 
-            if r < 5 and g < 5 and b < 5:
+            if r < 7 and g < 7 and b < 7:
                 image.set_at((px, py), (0, 0, 0, 0))
+
     # 5. Aplica o redimensionamento, se houver
     if scale != 1:
         novo_largura = int(width * scale)
